4e78665b497fe70c68cbf7aab60e233cb9580565
Elina <elspamur@gmail.com>
2024-02-28 12:04:59 +0300

    added difficult value

--- src/consts/settings.ts
+++ src/consts/settings.ts
@@ -1,5 +1,6 @@
+import { ref } from 'vue'
 import type { Size } from '@/types/size'
 
-export const CYCLE_INTERVAL = 1000
+export const CYCLE_INTERVAL = ref<number> (2000)
 
 export const FIELD_SIZE: Size = { x: 9, y: 16 }
