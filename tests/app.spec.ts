c8c2c651e01d40656be1285fa067570af2a5f15e
Elina <elspamur@gmail.com>
2024-03-04 12:56:55 +0300

    fix lint

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,31 +1,14 @@
 import { beforeEach, describe, expect, it } from 'vitest'
 import type { VueWrapper } from '@vue/test-utils'
 import { mount } from '@vue/test-utils'
-import { createRouter, createWebHistory } from 'vue-router'
 
-import { h } from 'vue'
 import App from '@/App.vue'
-import { RouteName } from '@/shared/consts/route-name'
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', component: h('div') },
-    ...Object.values(RouteName)
-      .map(name => ({
-        name,
-        path: `/${name.toLowerCase()}`,
-        component: h('div'),
-      })),
-  ],
-})
 
 describe('App', () => {
   let wrapper: VueWrapper<any>
 
-  beforeEach(async () => {
-    wrapper = mount(App, { global: { plugins: [router] } })
-    await router.isReady()
+  beforeEach(() => {
+    wrapper = mount(App)
   })
 
   it('should be visible', () => {
